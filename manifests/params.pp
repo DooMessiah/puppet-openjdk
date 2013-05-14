cca2231c0358cb32dc051c792941d979b22ba85e
Ash Caire <ash.caire@gmail.com>
2013-05-14 16:12:22 +1000

    add Ubuntu pkg name for openjdk module

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,9 @@ class openjdk::params {
     'debian': {
       $packageheadless = 'openjdk-6-jre-headless'
     }
+    'ubuntu': {
+      $packageheadless = 'openjdk-7-jre-headless'
+    }
     default: {
       notice("${::lsbdistid} not supported")
     }
